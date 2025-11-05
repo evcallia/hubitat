dca5bc3900c593aed1c88b78f7c7ef3b5109ccbc
evan.callia <calliaevan12@gmail.com>
2025-11-04 22:30:52 -0800

    update date

--- apps/laundry-notifications/laundry-notifications-parent.groovy
+++ apps/laundry-notifications/laundry-notifications-parent.groovy
@@ -25,7 +25,7 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License
  *  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *
- *  Last modified: 2024-04-04
+ *  Last modified: 2025-11-04
  */
 
 definition(
